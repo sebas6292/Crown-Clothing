55978ddcc609c286aa02d8aa5661142c4ec7ca4c
Sebastian  Leal <sebastianleal@Sebastians-Air.T-mobile.com>
2022-04-18 13:34:13 -0700

    architected the drop down to toggle but we still cant add products to our page

--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { UserProvider } from './contexts/user.context';
 import { ProductsProvider } from './contexts/products.context';
+import { CartProvider } from './contexts/cart.context';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider> 
         <ProductsProvider>
-          <App />
+          <CartProvider>
+            <App />
+          </CartProvider>
         </ProductsProvider>
       </UserProvider>  
     </BrowserRouter>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,17 +1,20 @@
 import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
+
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
 import './navigation.styles.scss';
 import { UserContext } from '../../contexts/user.context'
+import { CartContext } from '../../contexts/cart.context'
 
 import { signOutUser } from '../../utils/firebase.utils'
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
          // helps a user signout of the account by grabbing the user from the contextUser
+         const { isCartOpen } = useContext(CartContext);
     return (
     <Fragment>
         <div className='navigation'>
@@ -36,7 +39,7 @@ const Navigation = () => {
                     )}
                     <CartIcon />
             </div>
-            <CartDropdown />
+            { isCartOpen && <CartDropdown /> }
         </div>
         <Outlet />
     </Fragment> 
