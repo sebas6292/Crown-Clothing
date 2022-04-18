ebede88565f6d3daf4d606520c29ffbf1a347de7
Sebastian  Leal <sebastianleal@Sebastians-Air.lan>
2022-04-18 11:56:50 -0700

    added a cart icon and a dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,5 +1,7 @@
 import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
 import './navigation.styles.scss';
@@ -32,7 +34,9 @@ const Navigation = () => {
                             SIGN IN
                         </Link>
                     )}
+                    <CartIcon />
             </div>
+            <CartDropdown />
         </div>
         <Outlet />
     </Fragment> 
