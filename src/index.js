81350bcfc0f0a9a78674cf32a6ca0970f34e1329
Sebastian  Leal <sebastianleal@Sebastians-Air.lan>
2022-05-05 13:05:53 -0700

    debugging

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.scss';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { UserProvider } from './contexts/user.context';
 import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
+
+import './index.scss';
 
 const rootElement = document.getElementById('root');
 
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import './shop.styles.scss';
-import CategoryPreview from '../../components/category-preview/category-preview.component';
+import CategoriesPreview  from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 
 const Shop = () => {
     return (
        <Routes>
-           <Route index element={<CategoryPreview />} />
+           <Route index element={<CategoriesPreview />} />
            <Route path=":category" element={<Category />}/>
        </Routes>
     );
