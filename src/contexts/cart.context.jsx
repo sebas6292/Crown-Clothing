7cb10f119130f6af0a3c6868dfecf70a5916e145
Sebastian  Leal <sebastianleal@Sebastians-Air.lan>
2022-05-16 09:47:50 -0700

    last update

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,5 +1,7 @@
 import { createContext, useReducer } from "react";
 
+import { createAction } from '../utils/reducer/reducer.utils';
+
 const addCartItem = (cartItems, productToAdd) => {
     const existingCartItem = cartItems.find(
         (cartItem) => cartItem.id === productToAdd.id
@@ -105,20 +107,21 @@ export const CartProvider = ({children}) => {
                  0
         );
         
-        dispatch({ 
-            type: CART_ACTION_TYPES.SET_CART_ITEMS, 
-            payload: { 
+        dispatch(
+            createAction(CART_ACTION_TYPES.SET_CART_ITEMS, {
                 cartItems: newCartItems, 
                 cartTotal: newCartTotal, 
-                cartCount: newCartCount 
-        }});
-    }
+                cartCount: newCartCount,
+            })
+        );
+    };
 
     const setIsCartOpen = (bool) => {
-        dispatch({ 
-            type: CART_ACTION_TYPES.SET_IS_CART_OPEN, 
-            payload: bool 
-        });
+        dispatch(
+            createAction(
+                CART_ACTION_TYPES.SET_IS_CART_OPEN, 
+                bool)
+            )
     }
 
     const value = { 
