9496ef22fee50c3d6423cc006a9bbf0923ddd7fd
Sebastian  Leal <sebastianleal@Sebastians-Air.lan>
2022-07-07 12:51:38 -0700

    implentation of reducers

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,13 @@
 import { Fragment, useContext } from 'react';
 import { Outlet } from 'react-router-dom';
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
 
-import { UserContext } from '../../contexts/user.context'
+import { selectCurrentUser } from '../../store/user/user.selector';
 import { CartContext } from '../../contexts/cart.context'
 
 import { signOutUser } from '../../utils/firebase/firebase.utils'
@@ -20,8 +20,7 @@ import {
  } from './navigation.styles.jsx';
 
 const Navigation = () => {
-    const { currentUser } = useContext(UserContext);
-         // helps a user signout of the account by grabbing the user from the contextUser
+    const currentUser = useSelector(selectCurrentUser)
     const { isCartOpen } = useContext(CartContext);
 
     return (
