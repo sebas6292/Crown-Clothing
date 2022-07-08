28e2929d116f63188cc35f86b1651e19a961e933
Sebastian  Leal <sebastianleal@Sebastians-Air.lan>
2022-07-08 14:47:09 -0700

    conversion

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -6,17 +6,18 @@ import './shop.styles.scss';
 import CategoriesPreview  from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategoriesMap } from '../../store/categories/category.action.js';
+import { setCategories } from '../../store/categories/category.action.js';
 
 const Shop = () => {
-
     const dispatch = useDispatch();
 
     useEffect(() => {
         const getCategoriesMap = async () => {
-            const categoryMap = await getCategoriesAndDocuments();
-            dispatch(setCategoriesMap(categoryMap));
+            const categoriesArray = await getCategoriesAndDocuments('categories');
+            console.log(categoriesArray)
+            dispatch(setCategories(categoriesArray));
         }
+
         getCategoriesMap();
     }, []);
 
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -67,13 +67,7 @@ export const getCategoriesAndDocuments = async () => {
     const q = query(collectionRef);
 
     const querySnapshot = await getDocs(q);
-    const categoryMap = querySnapshot.docs.reduceRight((acc, docSnapshot) => {
-        const { title, items } = docSnapshot.data();
-        acc[title.toLowerCase()] = items;
-        return acc;
-    }, {});
-
-    return categoryMap;
+   return querySnapshot.docs.map(docSnapshot => docSnapshot.data())    
 }
 
 export const createUserDocumentFromAuth = async ( // grabbing the data and storing into firebase
