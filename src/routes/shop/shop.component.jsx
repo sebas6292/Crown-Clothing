8e419eb58f265e90c44f33216448e300cc05e0e3
Sebastian Leal <sebas6292@gmail.com>
2022-10-26 18:07:24 -0700

    applied styling components

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,17 +1,16 @@
 import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-// import './shop.styles.scss';
 
 import CategoriesPreview  from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
-import { fetchCategoriesStartAsync } from '../../store/categories/category.action.js';
+import { fetchCategoriesAsync } from '../../store/categories/category.action.js';
 
 const Shop = () => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        dispatch(fetchCategoriesStartAsync());
+        dispatch(fetchCategoriesAsync());
     }, []);
 
     return (
--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,6 +1,6 @@
 import { CATEGORIES_ACTION_TYPES } from "./category.types";
-
 import { createAction } from "../../utils/reducer/reducer.utils";
+
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 
 export const fetchCategoriesStart = () => {
@@ -15,7 +15,7 @@ export const fetchCategoriesFailed = (error) => {
     createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
 }
 
-export const fetchCategoriesStartAsync = () => async (dispatch) => {
+export const fetchCategoriesAsync = () => async (dispatch) => {
     dispatch(fetchCategoriesStart());
     try {
         const categoriesArray = await getCategoriesAndDocuments('categories');
@@ -23,5 +23,4 @@ export const fetchCategoriesStartAsync = () => async (dispatch) => {
     } catch (error) {
         dispatch(fetchCategoriesFailed(error));
     }
-    
 }
